a0262ceae97f710125cd7b1ee03dc57f18197a1f
satwaihlaing <ericjam2010@gmail.com>
2020-06-18 17:08:18 +0630

    fix image not showing on detail

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -36,7 +36,7 @@
             <div class="row">
                 <div class="col-xl-3 col-lg-4 col-md-5">
                     <a href="#" title="See covers">
-                        <img class="rounded" width="100%" src="https://mangadex.org{{ $image[0] }}">
+                        <img class="rounded" width="100%"  src="{{ $image[0] }}">
                     </a>
                 </div>
                 <div class="col-xl-9 col-lg-8 col-md-7">
